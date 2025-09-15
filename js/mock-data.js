53b47f25caebc6bb97b69321aa3553f7481252e9
lam <riley280120@gmail.com>
2025-09-15 17:32:45 +0700

    feat: product-list

--- js/mock-data.js
+++ js/mock-data.js
@@ -1,31 +1,31 @@
 const products = [
   {
-    category: " Cà phê",
+    category: "Cà phê",
     list: [
       {
         img: "https://png.pngtree.com/png-clipart/20230731/original/pngtree-black-coffee-with-ice-and-high-ball-glass-png-image_9380914.png",
         name: "Đen đá",
-        price: "30K",
+        price: 30000,
       },
       {
         img: "https://png.pngtree.com/png-clipart/20250209/original/pngtree-a-glass-of-iced-coffee-with-milk-and-ice-cubes-png-image_19650262.png",
         name: "Bạt xỉu truyền thống",
-        price: "30K",
+        price: 30000,
       },
       {
         img: "https://png.pngtree.com/png-clipart/20250103/original/pngtree-iced-coffee-glass-with-3d-splash-png-image_18670735.png",
         name: "Bạt xỉu va-ni",
-        price: "30K",
+        price: 30000,
       },
       {
         img: "https://png.pngtree.com/png-vector/20250416/ourlarge/pngtree-glass-of-coffee-latte-on-an-isolated-png-image_16002561.png",
         name: "Cà phê latte",
-        price: "35K",
+        price: 35000,
       },
       {
         img: "https://img.pikbest.com/origin/09/24/93/61ApIkbEsTVRK.png!w700wp",
         name: "Cold brew",
-        price: "38K",
+        price: 38000,
       },
     ],
   },
@@ -35,22 +35,22 @@ const products = [
       {
         img: "https://phela.vn/wp-content/uploads/2021/07/14369.jpg",
         name: "O long đậm vị",
-        price: "28K",
+        price: 28000,
       },
       {
         img: "https://glofood.vn/upload/baiviet/7dab210981b359ed00a21-2971.jpg",
         name: "O long gạo rang",
-        price: "28K",
+        price: 28000,
       },
       {
         img: "https://bizweb.dktcdn.net/100/421/036/files/cach-nau-tra-sua-truyen-thong-de-ban-2.jpg?v=1616644748197",
         name: "Truyền thống",
-        price: "25K",
+        price: 25000,
       },
       {
         img: "https://123coffee.vn/wp-content/uploads/2023/09/Tra-Sua-Ba-Tuoc.png",
         name: "Bá tước",
-        price: "28K",
+        price: 28000,
       },
     ],
   },
@@ -60,22 +60,22 @@ const products = [
       {
         img: "https://recipesblob.oetker.de/assets/a106928ad58e4c56bb072edce6ed87af/750x750/iced-vanilla-matcha.webp",
         name: "Matcha lát te",
-        price: "35K",
+        price: 35000,
       },
       {
         img: "https://product.hstatic.net/1000075078/product/1745246651_matcha-dao_8c749d752e0d4d2b85f0a03310bd1379_large.png",
         name: "Matcha bưởi hồng",
-        price: "35K",
+        price: 35000,
       },
       {
         img: "https://cdn.tgdd.vn/Files/2024/03/26/1563635/cach-lam-nuoc-dua-matcha-la-mieng-thu-mot-lan-la-nghien-ngay-202403261039460072.jpg",
         name: "Matcha and coco",
-        price: "35K",
+        price: 35000,
       },
       {
         img: "https://xachtaynhat.net/wp-content/uploads/2019/08/Matcha-latte.jpg",
         name: "Matcha sữa dừa ",
-        price: "35K",
+        price: 35000,
       },
     ],
   },
@@ -85,25 +85,25 @@ const products = [
       {
         img: "https://coffee.vntheme.com/wp-content/uploads/2022/09/TRA-DAO-CAM-SA.png",
         name: "Đào cam xả ",
-        price: "25K",
+        price: 25000,
       },
       {
         img: "https://goldenfarm.com.vn/wp-content/uploads/dao-nhai-hibicus-1.webp",
         name: "Hi-bi-cus mix bưởi",
-        price: "25K",
+        price: 25000,
       },
       {
         img: "https://nuwacoffee.com/wp-content/uploads/2024/03/Tra-Sen-Vang-La-Nep-65k-v.png",
         name: "Trà thư giản",
-        price: "25K",
+        price: 25000,
       },
       {
         img: "https://glofood.vn/upload/images/TRA%20VAI%20HOA%20HONG(2).png",
         name: "Yu-zu vải",
-        price: "30K",
+        price: 30000,
       },
     ],
   },
 ];
 
-export default products;
+export { products };
